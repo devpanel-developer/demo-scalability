a7c248d8ab01fd0f6de201ad42431dc80ad05ff8
Developer DevPanel <developer@devpanel.com>
2025-05-06 04:47:39 +0000

    Add Style

--- .devpanel/deployment.sh
+++ .devpanel/deployment.sh
@@ -2,4 +2,41 @@
 # For example, If you create a 4-container deployment, then each of those 4 containers will run this file at startup.
 # If you need to clear (or rebuild) cache, then put those commands here.
 
-echo 'Run deployment.sh....'
+STATIC_FILES_PATH="$WEB_ROOT/sites/default/files/"
+SETTINGS_FILES_PATH="$WEB_ROOT/sites/default/settings.php"
+OVERWRITE_SETTING="$APP_ROOT/.devpanel/.devpanel-drupal-overwrite-settings"
+
+if [[ ! -n "$APACHE_RUN_USER" ]]; then
+  export APACHE_RUN_USER=www-data
+fi
+if [[ ! -n "$APACHE_RUN_GROUP" ]]; then
+  export APACHE_RUN_GROUP=www-data
+fi
+
+#== Extract static files
+if [[ $(mysql -h$DB_HOST -P$DB_PORT -u$DB_USER -p$DB_PASSWORD $DB_NAME -e "show tables;") == '' ]]; then
+  if [[ -f "$APP_ROOT/.devpanel/dumps/files.tgz" ]]; then
+    echo  'Extract static files ...'
+    sudo mkdir -p $STATIC_FILES_PATH
+    sudo tar xzf "$APP_ROOT/.devpanel/dumps/files.tgz" -C $STATIC_FILES_PATH
+  fi
+
+  #== Import mysql files
+  if [[ -f "$APP_ROOT/.devpanel/dumps/db.sql.tgz" ]]; then
+    echo  'Extract mysql files ...'
+    SQLFILE=$(tar tzf $APP_ROOT/.devpanel/dumps/db.sql.tgz)
+    tar xzf "$APP_ROOT/.devpanel/dumps/db.sql.tgz" -C /tmp/
+    mysql -h$DB_HOST -P$DB_PORT -u$DB_USER -p$DB_PASSWORD $DB_NAME < /tmp/$SQLFILE
+    rm /tmp/$SQLFILE
+  fi
+fi
+
+#== Update permission
+echo 'Update permission ....'
+drush en hello_world -y
+drush en deployment -y
+drush cr
+sudo chown -R $APACHE_RUN_USER:$APACHE_RUN_GROUP $STATIC_FILES_PATH
+sudo chown www:www $SETTINGS_FILES_PATH
+sudo chmod 644 $SETTINGS_FILES_PATH
+
