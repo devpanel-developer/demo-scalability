19fc52c243a898f0467fb23759c307474d8ca902
Developer DevPanel <developer@devpanel.com>
2025-05-06 04:38:47 +0000

    Add Style

--- .devpanel/quickstart/create_quickstart.sh
+++ .devpanel/quickstart/create_quickstart.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # ---------------------------------------------------------------------
-# Copyright (C) 2021 DevPanel
+# Copyright (C) 2024 DevPanel
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
@@ -24,15 +24,9 @@ echo -e "-------------------------------\n"
 WORK_DIR=$APP_ROOT
 TMP_DIR=/tmp/devpanel/quickstart
 DUMPS_DIR=$TMP_DIR/dumps
-STATIC_FILES_DIR=$WEB_ROOT/$(drush php-eval "echo \Drupal\Core\StreamWrapper\PublicStream::basePath();") # Drupal 8 ways
+STATIC_FILES_DIR=$WEB_ROOT/sites/default/files
 
 mkdir -p $DUMPS_DIR
-#== Install needed tool
-if [[ ! -n $(which yq) ]]; then
-    echo -e "> Install needed tool"
-    wget https://github.com/mikefarah/yq/releases/download/v4.2.0/yq_linux_amd64.tar.gz -O - -q | \
-    tar xz && sudo mv yq_linux_amd64 /usr/bin/yq
-fi
 
 # Step 1 - Compress drupal database
 cd $WORK_DIR
@@ -55,25 +49,3 @@ tar czf $DUMPS_DIR/files.tgz -C $STATIC_FILES_DIR .
 echo -e "> Store files.tgz to $APP_ROOT/.devpanel/dumps"
 mkdir -p $APP_ROOT/.devpanel/dumps
 mv $DUMPS_DIR/files.tgz $APP_ROOT/.devpanel/dumps/files.tgz
-
-# Step 3 - Commit dumps files to git
-if [[ -f "$APP_ROOT/.devpanel/dumps/files.tgz" ]]; then
-    git add $APP_ROOT/.devpanel/dumps/files.tgz &> /dev/null
-fi
-
-if [[ -f "$APP_ROOT/.devpanel/dumps/db.sql.tgz" ]]; then
-    git add $APP_ROOT/.devpanel/dumps/db.sql.tgz &> /dev/null
-fi
-
-if [[ -f "$APP_ROOT/.devpanel/quickstart/quickstart.url" ]]; then
-    git add $APP_ROOT/.devpanel/quickstart/quickstart.url &> /dev/null
-fi
-
-export GIT_AUTHOR_NAME="DevPanel Bot"
-export GIT_AUTHOR_EMAIL="admin@devpanel.com"
-export GIT_COMMITTER_NAME="DevPanel Bot"
-export GIT_COMMITTER_EMAIL="admin@devpanel.com"
-
-git commit -m "Generate quickstart" &> /dev/null
-
-git push origin $GIT_BRANCH &> /dev/null
