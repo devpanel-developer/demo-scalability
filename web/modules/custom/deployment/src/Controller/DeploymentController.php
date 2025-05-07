0e54e22fed4e2abf828691b874b3f098c7534b38
Developer DevPanel <developer@devpanel.com>
2025-05-07 03:02:53 +0000

    Update code

--- web/modules/custom/deployment/src/Controller/DeploymentController.php
+++ web/modules/custom/deployment/src/Controller/DeploymentController.php
@@ -7,7 +7,7 @@ use Drupal\Core\Controller\ControllerBase;
 class DeploymentController extends ControllerBase {
   public function index() {
     return [
-      '#markup' => '<div class="deployment-blue-style"><h1>Blue Deployment</h1></div>',
+      '#markup' => '<div class="deployment-blue-style"><h1>Green Deployment</h1></div>',
       '#attached' => [
         'library' => [
           'deployment/styles',
